98d02f39e7cfd6a7d958fbbde108ba3b8b756a0e
swyx <shawnthe1@gmail.com>
2016-11-28 04:33:52 +0000

    fix

--- demo.js
+++ demo.js
@@ -2,3 +2,6 @@ var express = require('express')
 var app = express();
 app.get("/", function(request, response) {  response.end("Welcome to the homepage!");})
 app.get("/new/:url", function(req, res) {res.end("Hello, " + req.params.url + ".");})
+
+
+app.listen(process.env.PORT || 3000);
