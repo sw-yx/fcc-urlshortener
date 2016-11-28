f3788f687b66661906e2c5310abf79ee53de1a2a
swyx <shawnthe1@gmail.com>
2016-11-28 04:26:34 +0000

    init

---
+++ demo.js
@@ -0,0 +1,4 @@
+var express = require('express')
+var app = express();
+app.get("/", function(request, response) {  response.end("Welcome to the homepage!");})
+app.get("/new/:url", function(req, res) {res.end("Hello, " + req.params.url + ".");})
