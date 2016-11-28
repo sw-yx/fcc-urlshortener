98736ff90168f4e76d661d4f0cb6983655cf8afc
swyx <shawnthe1@gmail.com>
2016-11-28 04:40:08 +0000

    route*

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 app.get("/", function(request, response) {  response.end("Welcome to the homepage!");})
-app.get("/new/:url", function(req, res) {res.end("Hello, " + req.params.url + ".");})
+app.get("/new/:url*", function(req, res) {res.end("Hello, " + req.params.url + ".");})
 
 
 app.listen(process.env.PORT || 3000);
